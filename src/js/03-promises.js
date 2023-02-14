0c4237b4dd6794f51104149670df421c28be5553
oolga77 <oemi.ua@gmail.com>
2023-02-14 16:37:47 +0200

    required 03-promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,59 +1,44 @@
-import { Notify } from 'notiflix';
-
-const refs = {
-formData: document.querySelector('.form'),
-firstDelay: document.querySelector('[name = "delay"]'),
-delayStep: document.querySelector('[name = "step"]'),
-delayAmount: document.getElementsByName('[name = "amount"]'),
-btnCreatePromise: document.querySelector('button[type=submit]')
+ import { Notify } from 'notiflix';
+ const refs = {
+  formData: document.querySelector('.form'),
+  firstDelay: document.querySelector('[name=delay]'),
+  stepDelay: document.querySelector('[name=step]'),
+  amountDelay: document.querySelector('[name=amount]'),
+  createPromiseeBtn: document.querySelector('[type=submit]'),
 }
 
-refs.btnCreatePromise.addEventListener('submit', evt => {
-  evt.preventDefault();
-})
+  refs.createPromiseeBtn.addEventListener('click', event => {
+  event.preventDefault();
+  
+    let delay = Number(refs.firstDelay.value);
+    let delayStep = Number(refs.stepDelay.value);
+    let delayAmount = Number(refs.amountDelay.value);
+    for (let position = 1; position <= delayAmount; position++) {
+      createPromise(position, delay)
+        .then(({ position, delay }) => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+          
+        });
+      delay += delayStep;
+    }
+    refs.formData.reset();
+  });
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
 
-return new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (shouldResolve) {
-      resolve({ position, delay });
-    } else {
-      reject({ position, delay });
-    }
-  }, delay);
-});
-};
-
-refs.btnCreatePromise.addEventListener('click', onSubmit);
-
-
-function onSubmit(){
- 
-
-  let delay = Number(refs.firstDelay.value);
-  let step = Number(refs.delayStep.value);
-  let amount = Number(refs.delayAmount.value);
-
-  for (let i = 0; i <= amount; i++) {
-    createPromise(i, delay)
-      .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-    delay += step;
-  }
-  
-  
-refs.formData.reset();
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
-
-
-
-
 
